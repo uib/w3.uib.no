93bf0b267eb87fb31c8a4a7b2e14a7c5003bfd2e
Gisle Aas <gisle.aas@it.uib.no>
2015-06-10 19:59:26 +0200

    RTS-10822 Modify the filtering behaviour
    When no checkboxes are selected we should do no filtering.
    It has the same semantic as checking them all.
    
    This also sets a class on the date entries when they collapse
    that can be used to style that state.

--- modules/uib_calendar3/cal3.js
+++ modules/uib_calendar3/cal3.js
@@ -2,28 +2,47 @@ jQuery(document).ready(function($) {
   $('.calendar-types input').click(function() {
     var $type = $(this).data('type');
     var $checked = this.checked;
-    $('.event-entry-' + $type).toggle($checked);
-    if ($type == 'course') {
-      // toggle them all
-      $('.calendar-types input').each(function() {
-        var $t2 = $(this).data('type');
-        if ($t2.substr(0, 7) == 'course_') {
-          this.checked = $checked;
-          $('.event-entry-' + $t2).toggle($checked);
-        }
-      });
+    var $count_checked = 0;
+    $('.calendar-types input').each(function() {
+      if (this.checked) {
+        $count_checked++;
+      }
+    });
+    if (!$count_checked) {
+      // make them all visible
+      $('.event-entry').toggle(true);
+      $('.calendar-date').removeClass('collapsed');
+      return;
     }
+    if ($checked && $count_checked == 1) {
+      // everyone was visible before
+      $('.event-entry').toggle(false);
+    }
+    $('.event-entry-' + $type).toggle($checked);
+
+    // XXX recalculate collapsed state
+    $('.calendar-date').each(function() {
+      var v = $(this);
+      v.toggleClass('collapsed', v.has('.event-entry:visible').length == 0);
+    });
   });
   $('#hide-events').click(function() {
+    $('.event-entry').toggle(false);
+    $('.calendar-date').addClass('collapsed');
     $('.calendar-types input').each(function() {
       this.checked = false;
-      var $type = $(this).data('type');
-      $('.event-entry-' + $type).toggle(false);
     });
   });
   $('.calendar-date h3').click(function() {
-    $(this).parent().children('div').each(function() {
-      $(this).toggle(true);
+    var container = $(this).parent();
+    var hide = container.toggleClass('collapsed').hasClass('collapsed');
+    container.children('.event-entry').each(function() {
+      if (hide) {
+        $(this).slideUp();
+      }
+      else {
+        $(this).slideDown();
+      }
     });
   });
 
