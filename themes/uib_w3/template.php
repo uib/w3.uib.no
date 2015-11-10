0222d0361c593b149f171a44850175dda28e61c8
Amund Østvoll <amund.ostvoll@adm.uib.no>
2015-11-10 11:49:59 +0100

    RTS-12876 Rendering contact information in footer

--- themes/uib_w3/template.php
+++ themes/uib_w3/template.php
@@ -123,6 +123,7 @@ function uib_w3_preprocess_page(&$variables, $hook) {
       ));
       $variables['page']['footer_top']['uib_area_jobbnorge'] = __uib_w3__render_block('uib_area','jobbnorge',20);
       $variables['page']['footer_top']['field_uib_feed'] = __uib_w3__render_block('uib_area', 'feed', 15);
+      $variables['page']['footer']['uib_area_contact_colophon'] = __uib_w3__render_block('uib_area','colophon_2',15);
 
       switch ($variables['node']->field_uib_area_type['und'][0]['value']) {
         case 'newspage':
