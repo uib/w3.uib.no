297126ce01e22c0f0e60e1c48ad52b1e8ed9feb3
Jonas F Henriksen <jonas.henriksen@uib.no>
2015-11-19 15:49:55 +0100

    RTS-13144 Added functionality to close search overlay by pressing esc.

--- themes/uib_w3/js/w3.js
+++ themes/uib_w3/js/w3.js
@@ -1,20 +1,33 @@
-jQuery(document).ready(function($){
-  $('.block-uib-search form[name=noscriptform] .search-button').click(function(event){
-    event.preventDefault();
-    if($('#uib-search-lightbox-bottom').length==0){
-      $('.block-uib-search form[name=lbform]').appendTo(
-        $('<div id="uib-search-lightbox-bottom" class="block-uib-search"/>').appendTo('body')
-      );
-    }
-    $('.block-uib-search .lightbox').css('display','block');
-    $('.block-uib-search .lightbox .search-field').focus();
-    $('.block-uib-search .lightbox').animate({
-      'opacity': .8
-    }, 300);
+(function ($) {
+  $(document).ready(function($){
+    $('.block-uib-search form[name=noscriptform] .search-button').click(function(event){
+      event.preventDefault();
+      if($('#uib-search-lightbox-bottom').length==0){
+        $('.block-uib-search form[name=lbform]').appendTo(
+          $('<div id="uib-search-lightbox-bottom" class="block-uib-search"/>').appendTo('body')
+        );
+      }
+      $('.block-uib-search .lightbox').css('display','block');
+      $('.block-uib-search .lightbox .search-field').focus();
+      $('.block-uib-search .lightbox').animate({
+        'opacity': .8
+      }, 300);
+    });
+    $('.closeme').click(function(event){
+      event.preventDefault();
+      uibSearchClose();
+    })
+    $(document).keyup(function(e) {
+      if (e.which == 27) {
+        if($('#uib-search-lightbox-bottom').length==1 &&
+          $('.block-uib-search .lightbox').css('display')!='none'){
+          uibSearchClose();
+        }
+      }
+    });
   });
-  $('.closeme').click(function(event){
-    event.preventDefault();
-    $('.block-uib-search .lightbox').css('display','none');
-    $('.block-uib-search .lightbox').css('opacity','0');
-  })
-});
+  function uibSearchClose(){
+      $('.block-uib-search .lightbox').css('display','none');
+      $('.block-uib-search .lightbox').css('opacity','0');
+  }
+})(jQuery);
