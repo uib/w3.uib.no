45fb91a10563a81de9834440ea4fd6fa564bdd34
Are Johannessen <are.johannessen@uib.no>
2015-12-07 13:21:59 +0100

    RTS-13396 Updated article and external content short teaser templates
    - Moved media first in the template
    - Added a div around the rest of the content

--- themes/uib_w3/templates/node--article--short-teaser.tpl.php
+++ themes/uib_w3/templates/node--article--short-teaser.tpl.php
@@ -79,46 +79,15 @@
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <div class="content clearfix"<?php print $content_attributes; ?>>
-    <?php print render($title_prefix); ?>
+  <?php print render($content['field_uib_main_media']); ?>
+  <div class="relation_content">
     <?php if (!$page): ?>
       <h2<?php print $title_attributes; ?>>
         <a href="<?php print $node_url; ?>"><?php print $title; ?></a>
       </h2>
     <?php endif; ?>
-    <?php print render($title_suffix); ?>
-
-    <?php if ($display_submitted): ?>
-      <div class="meta submitted">
-        <?php print $user_picture; ?>
-        <?php print $submitted; ?>
-      </div>
-    <?php endif; ?>
-
     <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
       print render($content);
     ?>
   </div>
-
-  <?php
-    // Remove the "Add new comment" link on the teaser page or if the comment
-    // form is being displayed on the same page.
-    if ($teaser || !empty($content['comments']['comment_form'])) {
-      unset($content['links']['comment']['#links']['comment-add']);
-    }
-    // Only display the wrapper div if there are links.
-    $links = render($content['links']);
-    if ($links):
-  ?>
-    <div class="link-wrapper">
-      <?php print $links; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php print render($content['comments']); ?>
-
 </div>
