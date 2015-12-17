1e1e4c400ac3ac1a794749bc8bf4140a005a4499
Jonas F Henriksen <jonas.henriksen@uib.no>
2015-12-17 08:42:52 +0100

    RTS-13445 Applying improved js from styleguide

--- themes/uib_w3/js/w3.js
+++ themes/uib_w3/js/w3.js
@@ -25,14 +25,30 @@
         }
       }
     });
-    $('#main-menu .menu .menu__item').hover(
+    if(!$('#main-menu .menu .menu__item').hasClass('jstransition')){
+      $('#main-menu .menu .menu__item').addClass('jstransition');
+    }
+    $('#main-menu .menu .menu__item.jstransition').hover(
       function(e){
         $(this).children('ul').first().clearQueue();
         $(this).children('ul').first().stop().delay(200).animate(
-          {height: $(this).children('ul').first().get(0).scrollHeight}, 500 );
+          {
+            height: $(this).children('ul').first().get(0).scrollHeight+80,
+            minHeight: '23.5rem',
+            paddingTop: '1.875rem',
+            paddingRight: '1.875rem',
+            paddingBottom: '1.875rem',
+          }, 400 );
     }, function(e){
       $(this).children('ul').first().clearQueue();
-      $(this).children('ul').first().stop().animate({height: 0}, 300 );
+      $(this).children('ul').first().stop().animate(
+        {
+          height: 0,
+          minHeight: 0,
+          paddingTop: 0,
+          paddingRight: 0,
+          paddingBottom: 0,
+        }, 300 );
     });
   });
   function uibSearchClose(){
