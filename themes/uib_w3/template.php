e167eae9454292094e5efa4198be7df5b2332d51
Jonas F Henriksen <jonas.henriksen@uib.no>
2016-01-20 09:34:36 +0100

    RTS-12878 Corrected date input.
    Also checking that date actually exists before writing calendar card.

--- themes/uib_w3/template.php
+++ themes/uib_w3/template.php
@@ -650,10 +650,11 @@ function uib_w3_preprocess_node(&$variables, $hook) {
         $variables['content']['field_uib_main_media'] = __uib_w3__keep_first_main_media($variables['content']['field_uib_main_media']);
       }
       $variables['theme_hook_suggestions'][] = 'node__article__teaser';
-      if( count($variables['field_uib_event_type'])){
+      if( count($variables['field_uib_event_type']) &&
+        isset($variables['field_uib_date']['und'][0]['value'])){
         $variables['content']['field_uib_main_media'][0]['#markup'].=
           uib_calendar3__get_calendar_card_render_array(
-          $variables['field_uib_date'][0]['value'] . 'Z');
+          $variables['field_uib_date']['und'][0]['value'] . 'Z');
       }
 
     }
