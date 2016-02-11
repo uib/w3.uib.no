234a3905167bcd874ad201c9248238b2008014ec
Pål Schroeder <pal.schroeder@uib.no>
2016-02-11 09:16:15 +0100

    RTS-12745 jstransition patch

--- themes/uib_w3/js/w3.js
+++ themes/uib_w3/js/w3.js
@@ -25,31 +25,33 @@
         }
       }
     });
-    if(!$('#main-menu .menu .menu__item').hasClass('jstransition')){
-      $('#main-menu .menu .menu__item').addClass('jstransition');
-    }
-    $('#main-menu .menu .menu__item.jstransition').hover(
-      function(e){
+    if(!$('.uib-area-expanded-menu').length) {
+      if(!$('#main-menu .menu .menu__item').hasClass('jstransition')){
+        $('#main-menu .menu .menu__item').addClass('jstransition');
+      }
+      $('#main-menu .menu .menu__item.jstransition').hover(
+        function(e){
+          $(this).children('ul').first().clearQueue();
+          $(this).children('ul').first().stop().delay(200).animate(
+            {
+              height: $(this).children('ul').first().get(0).scrollHeight+80,
+              minHeight: '23.5rem',
+              paddingTop: '1.875rem',
+              paddingRight: '1.875rem',
+              paddingBottom: '1.875rem',
+            }, 400 );
+      }, function(e){
         $(this).children('ul').first().clearQueue();
-        $(this).children('ul').first().stop().delay(200).animate(
+        $(this).children('ul').first().stop().animate(
           {
-            height: $(this).children('ul').first().get(0).scrollHeight+80,
-            minHeight: '23.5rem',
-            paddingTop: '1.875rem',
-            paddingRight: '1.875rem',
-            paddingBottom: '1.875rem',
-          }, 400 );
-    }, function(e){
-      $(this).children('ul').first().clearQueue();
-      $(this).children('ul').first().stop().animate(
-        {
-          height: 0,
-          minHeight: 0,
-          paddingTop: 0,
-          paddingRight: 0,
-          paddingBottom: 0,
-        }, 300 );
-    });
+            height: 0,
+            minHeight: 0,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+          }, 300 );
+      });
+    }
   });
   function uibSearchClose(){
       $('.block-uib-search .lightbox').css('display','none');
