0240a4ff909241c0a6975ba2c369bb83c7fad178
Are Johannessen <are.johannessen@uib.no>
2016-04-15 15:46:16 +0200

    RTS-14974 Alter link dialog options in ckeditor
    This commit removes the advanced tab and removes some options in the
    target tab for the link dialog in ckeditor.

--- modules/uib_ckeditor_config/ckeditor-config.js
+++ modules/uib_ckeditor_config/ckeditor-config.js
@@ -1,3 +1,18 @@
 CKEDITOR.editorConfig = function( config ) {
-
+  // config.linkShowAdvancedTab = false;
+  config.removeDialogTabs = 'link:advanced';
 };
+
+CKEDITOR.on( 'dialogDefinition', function( ev ) {
+  var dialogName = ev.data.name;
+  var dialogDefinition = ev.data.definition;
+
+  if ( dialogName == 'link' ) {
+    var targetTab = dialogDefinition.getContents( 'target' );
+    var targetField = targetTab.get( 'linkTargetType' );
+    targetField['items'] = [];
+    targetField['items'][0] = ['<not set>', 'notSet'];
+    targetField['items'][1] = ['New window (_blank)', '_blank'];
+    targetField['items'][2] = ['Same window (_self)', '_self'];
+  }
+});
