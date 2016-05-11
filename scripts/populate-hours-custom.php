d0d19a86e633398602df617bca741897885e2e75
Jonas F Henriksen <jonas.henriksen@uib.no>
2016-05-11 14:35:46 +0200

    RTS-15182 Wrapping content in <p>-tag to prevent visual changes when saving in wysiwyg

--- scripts/populate-hours-custom.php
+++ scripts/populate-hours-custom.php
@@ -2,7 +2,7 @@
 // This script updates field_data_field_uib_hours_custom fields from
 // the similar field field_data_field_uib_custom_hours. The field
 // field_data_field_uib_custom_hours has the wrong type, and will be
-// deleted in the next release. 
+// deleted in the next release.
 
 $sql = "select distinct n.nid from node n
   inner join field_data_field_uib_custom_hours h on (n.nid = h.entity_id)";
@@ -11,5 +11,8 @@ $list = db_query($sql)->fetchAll();
 foreach ($list as $key => $value) {
   $n = node_load($value->nid);
   $n->field_uib_hours_custom = $n->field_uib_custom_hours;
+  foreach ($n->field_uib_hours_custom as $lang => &$row) {
+    $row[0]['value'] = "<p>{$row[0]['value']}</p>";
+  }
   node_save($n);
 }
