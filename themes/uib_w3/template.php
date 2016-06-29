fc1dfa00b6a82e5f387a817ccf01d7cb8efe6f34
Are Johannessen <are.johannessen@uib.no>
2016-06-29 13:48:31 +0200

    RTS-15702 Made condition in uib_w3_menu_link more accurate

--- themes/uib_w3/template.php
+++ themes/uib_w3/template.php
@@ -1139,12 +1139,13 @@ function uib_w3_menu_link($variables) {
       return $link;
     }
   }
-  if ((strstr($variables['theme_hook_original'], 'menu_link__menu_global_menu_2')
-    || strstr($variables['theme_hook_original'], 'menu_link__menu_global_menu_no_2'))
-    && $variables['element']['#href'] == 'http:#') {
-    return str_replace('http:#', '#', theme_menu_link($variables));
-  }
-  else {
-    return theme_menu_link($variables);
+  if (strstr($variables['theme_hook_original'], 'menu_link__menu_global_menu_2')
+    || strstr($variables['theme_hook_original'], 'menu_link__menu_global_menu_no_2')) {
+    if ($variables['element']['#href'] == 'http:#') {
+      return str_replace('http:#', '#', theme_menu_link($variables));
+    }
+    else {
+      return theme_menu_link($variables);
+    }
   }
 }
