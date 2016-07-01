c7888e0be511675f2087baa5c597ffb080b19e54
Gisle Aas <gisle.aas@it.uib.no>
2016-07-01 13:39:25 +0200

    RTS-15642 Generate SQL for SEBRA to execute
    This will deal both users that should loose all privileges (RTS-15643)
    as we as those to be downgraded from 'level 2' (Redaktør) to 'level 3'
    (Innholdsprodusent).

--- scripts/stale-content-mgr.php
+++ scripts/stale-content-mgr.php
@@ -1,23 +1,57 @@
 <?php
 
-# This script lists users with roles that has not logged in since the given date.
-# It's used to remove their access with a manual update of the FD_OMRAADE_ROLLE
-# table in SEBRA.
+# This script outputs SQL that should be executed against the SEBRA database
+# in order to degrade user roles for those that don't use their privilege.
 #
 # See https://rts.uib.no/issues/15372
 
-$limit_2016 = mktime(0, 0, 0, 1, 1, 2016);
+$limit_level3 = mktime(0, 0, 0, 1, 1, 2016);
+$limit_level2 = mktime(0, 0, 0, 5, 1, 2016);
+
+if (0) {
+  # calculate relative limits
+  $month = 365.25 / 12 * 24 * 60 * 60;
+
+  $limit_level3 = time() - 6 * $month;
+  $limit_level2 = time() - 3 * $month;
+}
 
 $rid_level1 = 74573517;
 $rid_level2 = 170699807;
 $rid_level3 = 64272948;
 
-$result = db_query("SELECT DISTINCT name, login FROM {users} AS u JOIN users_roles AS r ON u.uid = r.uid JOIN field_data_field_uib_user_domain AS d ON u.uid = d.entity_id WHERE login < :lim AND field_uib_user_domain_value = 'uib' AND rid IN ($rid_level1, $rid_level2, $rid_level3) ORDER BY name", array(
-  ':lim' => $limit_2016,
+$result = db_query("SELECT DISTINCT name, login FROM {users} AS u JOIN users_roles AS r ON u.uid = r.uid JOIN field_data_field_uib_user_domain AS d ON u.uid = d.entity_id WHERE created < :lim AND login < :lim AND field_uib_user_domain_value = 'uib' AND rid IN ($rid_level1, $rid_level2, $rid_level3) ORDER BY name", array(
+  ':lim' => $limit_level3,
 ));
+
+$delete_users = array();
 foreach ($result as $user) {
-  $user->login = date("Y-m-d", $user->login);
-  print "'$user->name', ";
+  $seen[$user->name] = true;
+  $delete_users[] = "'$user->name'";
 }
 
-print "\n";
+if ($delete_users) {
+  $count = count($delete_users);
+  print "-- Innholdsprodusent ($count) that have not logged in since " . date("Y-m-d", $limit_level3) . "\n";
+  print "DELETE FROM fd_omraade_rolle WHERE pseq IN (SELECT pseq FROM konto WHERE navn in (" . implode(', ', $delete_users) . "));\n";
+}
+
+$result = db_query("SELECT DISTINCT name, login FROM {users} AS u JOIN users_roles AS r ON u.uid = r.uid JOIN field_data_field_uib_user_domain AS d ON u.uid = d.entity_id WHERE created < :lim AND login < :lim AND field_uib_user_domain_value = 'uib' AND rid IN ($rid_level1, $rid_level2) ORDER BY name", array(
+  ':lim' => $limit_level2,
+));
+
+$downgrade_users = array();
+foreach ($result as $user) {
+  if ($seen[$user->name])
+    continue;
+  $downgrade_users[] = "'$user->name'";
+}
+
+if ($downgrade_users) {
+  if ($delete_users)
+    $count = count($downgrade_users);
+    print "\n";
+  print "-- Downgrade Redaktør ($count) that have not logged in since " . date("Y-m-d", $limit_level2) . "\n";
+  print "UPDATE fd_omraade_rolle SET rolle = 'Innholdsprodusent' WHERE rolle = 'Redaktør' AND pseq IN (SELECT pseq FROM konto WHERE navn IN (" . implode(', ', $downgrade_users) . "));\n";
+
+}
