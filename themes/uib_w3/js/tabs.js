b6d60709f048cefe397011b3e2dd0e496c1aabb0
Pål Schroeder <pal.schroeder@uib.no>
2016-08-16 12:45:27 +0200

    RTS-15879 Keeping only mobile part of tabs script
    Also included is commit:f692e07 in SG

--- themes/uib_w3/js/tabs.js
+++ themes/uib_w3/js/tabs.js
@@ -1,22 +1,6 @@
 (function ($) {
   Drupal.behaviors.uib_tabs = {
     attach: function (context, settings) {
-      $('.uib-tabs-container').tabs({
-        show: function(event, ui) {
-          if (ui.panel.id) {
-            var initialScroll = $(window).scrollTop();
-            window.location.hash = ui.panel.id;
-            $(window).scrollTop(initialScroll);
-            $('#page').scrollTop(0);
-          }
-          else {
-            window.location.hash = "";
-            $(window).scrollTop(0);
-            $('#page').scrollTop(0);
-            $('html, body').animate({ scrollTop: 0 }, 1);
-          }
-        }
-      });
       $('.uib-tabs-nav-mobile').change(function() {
         var activeTab = $(this).find("option:selected").val();
         $('.offices.uib-tabs-container .ui-tabs-panel').addClass('ui-tabs-hide').hide();
