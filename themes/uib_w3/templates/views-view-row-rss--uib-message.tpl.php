70e4102d445f4d01e74b6c2a088dc1ff6bb82377
Amund Østvoll <amund.ostvoll@adm.uib.no>
2016-08-25 11:36:44 +0200

    RTS-16020 Link field changed to plain text and removal if empty

--- themes/uib_w3/templates/views-view-row-rss--uib-message.tpl.php
+++ themes/uib_w3/templates/views-view-row-rss--uib-message.tpl.php
@@ -9,12 +9,7 @@
 ?>
   <item>
     <title><?php print $title; ?></title>
-<?php
-$hypertext = explode('href', $link);
-$s = explode('%3E',$hypertext[1]);
-$p = str_replace(array('%22','%26','%3D','%3A','%3F'),array('','&','',':','?'),$s[0]);
-?>
-    <link><?php print $p?></link>
+    <?php if($link !== 'http:noresult') print('<link>' . $link . '</link>'); ?>
     <description><?php print $description; ?></description>
     <?php print $item_elements; ?>
   </item>
