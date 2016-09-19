1356ecb37756a8e3e090f03200814b1484a54908
Are Johannessen <are.johannessen@uib.no>
2016-09-19 14:21:35 +0200

    RTS-16250 New logic and structure for message block
    We did not want any messages to be displayed until a user is logged in.
    This commit aims to prepare the structure for further updates to
    features in the message box.

--- modules/uib_message/js/msg-block.js
+++ modules/uib_message/js/msg-block.js
@@ -18,64 +18,60 @@ jQuery( document ).ready(function ($) {
         var user = feideUser[0].split(":")[1].split("@")[0];
         var json = "/api/msg?user=" + user + "&limit=6&language="+language;
         $.getJSON(json, function(result){
-          outputAPI(result);
-          $(".uib-collapsible-content").toggle();
-          $(".uib-collapsible-handle").toggleClass('open closed');
-          $(".uib-collapsible-handle").html($(".uib-collapsible-handle").html() == 'Vis meldinger' ? 'Skjul meldinger' : 'Vis meldinger');
-          $(".uib-feide-login").text("Logg ut");
-          $(".uib-feide-login").click(function() {
-            jso.wipeTokens();
-            window.location.assign(location.origin + location.pathname);
+          $("#messages-block-content").text("");
+          $.each(result, function(i, field){
+            var json_obj = field;
+            var output = "<div class='uib-collapsible-container'>";
+            output += "<h2 class='uib-collapsible-handle closed'>Vis meldinger</h2>";
+            output += "<div class='uib-collapsible-content' style='display:none;'>";
+            output += "<ul>";
+            for (var i in json_obj) {
+              output += "<li>";
+              output +=  " <span class='message-tag'>" + Drupal.checkPlain(json_obj[i].tag) + "</span>"
+                     + " <span class='message-text'>" + Drupal.checkPlain(json_obj[i].text) + "</span>";
+              if(json_obj[i].link) {
+                output += " <span class='message-link'><a href='" + json_obj[i].link + "'>Read more...</a></span>";
+              }
+              output += "<span class='message-area'>" + Drupal.checkPlain(json_obj[i].area) + "</span>";
+              output += "</li>";
+            }
+            output += "</ul>";
+            output += "<div class='uib-feide-login'>";
+            output += "Logg inn";
+            output += "</div>";
+            output += "</div>";
+            output += "</div>";
+            $("#messages-block-content").append(output);
+            $(".uib-collapsible-handle").click(function(event){
+              $(".uib-collapsible-content").toggle();
+              $(".uib-collapsible-handle").toggleClass('open closed');
+              $(".uib-collapsible-handle").html($(".uib-collapsible-handle").html() == 'Vis meldinger' ? 'Skjul meldinger' : 'Vis meldinger');
+            });
+            $(".uib-feide-login").text("Logg ut");
+            $(".uib-feide-login").click(function() {
+              jso.wipeTokens();
+              window.location.assign(location.origin + location.pathname);
+            });
           });
         });
       }
     });
   }
   else {
-    var json = "/api/msg?area=IT-avdelingen&tag=1&limit=6&language="+language;
-    $.getJSON(json, function(result){
-      outputAPI(result);
+      $("#messages-block-content").text("");
+      var output = "<div class='uib-collapsible-container'>";
+      output += "<h2 class='uib-collapsible-handle open'>Meldinger</h2>";
+      output += "<div class='uib-collapsible-content'>"
+      output += "<div class='uib-feide-login'>";
+      output += "Logg inn";
+      output += "</div>";
+      output += "</div>"
+      $("#messages-block-content").append(output);
       $(".uib-feide-login").click(function() {
         jso.ajax({
           url: "https://auth.dataporten.no/userinfo",
           datatype: 'json',
         });
       });
-    });
-  }
-
-  function outputAPI(result){
-    $("#messages-block-content").text("");
-    $.each(result, function(i, field){
-      var json_obj = field;
-      var output = "<div class='uib-collapsible-container'>";
-      output += "<h2 class='uib-collapsible-handle closed'>Vis meldinger</h2>";
-      output += "<div class='uib-collapsible-content' style='display:none;'>";
-      output += "<ul>";
-      for (var i in json_obj) {
-        output += "<li>";
-        output +=  " <span class='message-tag'>" + Drupal.checkPlain(json_obj[i].tag) + "</span>"
-               + " <span class='message-text'>" + Drupal.checkPlain(json_obj[i].text) + "</span>";
-        if(json_obj[i].link) {
-          output += " <span class='message-link'><a href='" + json_obj[i].link + "'>Read more...</a></span>";
-        }
-        output += "<span class='message-area'>" + Drupal.checkPlain(json_obj[i].area) + "</span>";
-        output += "</li>";
-      }
-      output += "</ul>";
-      output += "<div class='uib-feide-login'>";
-      output += "Logg inn";
-      output += "</div>";
-      output += "</div>";
-      output += "</div>";
-      $("#messages-block-content").append(output);
-      $(".uib-collapsible-handle").css("cursor", "pointer");
-      $(".uib-feide-login").css("cursor", "pointer");
-      $(".uib-collapsible-handle").click(function(event){
-        $(".uib-collapsible-content").toggle();
-        $(".uib-collapsible-handle").toggleClass('open closed');
-        $(".uib-collapsible-handle").html($(".uib-collapsible-handle").html() == 'Vis meldinger' ? 'Skjul meldinger' : 'Vis meldinger');
-      });
-    });
   }
 });
