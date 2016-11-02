029c6366a0f128d278ef1c20d7153e9e2213fee9
Gisle Aas <gisle.aas@it.uib.no>
2016-11-02 14:19:54 +0100

    Dump cardinality and requiredness using regexp notation

--- scripts/dump-bundles.php
+++ scripts/dump-bundles.php
@@ -3,23 +3,38 @@
 # This script prints the definitions for the bundles that we have
 # defined in some imaginary Drupal programming language in the C family.
 
-$fields = field_info_field_map();
+$fields = field_info_fields();
+$instance_info_map = field_info_instances();
 
-$bundle = array();
+$bundle = array(); # our output
 
-foreach ($fields as $field => $info) {
-  foreach ($info['bundles'] as $type => $bundles) {
+foreach ($fields as $field_name => $field_info) {
+  foreach ($field_info['bundles'] as $type => $bundles) {
     foreach ($bundles as $bundle_name) {
+      $instance_info = $instance_info_map[$type][$bundle_name][$field_name];
       if (!isset($bundle[$bundle_name])) {
         $bundle[$bundle_name] = array(
           'type' => $type,
           'fields' => array(),
         );
       }
-      $field_short = preg_replace('/^field_uib_/', '_', $field);
+      $ftype = $field_info['type'];
+      if ($field_info['cardinality'] == -1) {
+        $ftype .= $instance_info['required'] ? "+" : "*";
+      }
+      elseif ($field_info['cardinality'] > 1) {
+        $ftype .= '{';
+        $ftype .= $instance_info['required'];
+        $ftype .= ',';
+        $ftype .= $field_info['cardinality'];
+        $ftype .= '}';
+      }
+      elseif (!$instance_info['required']) {
+          $ftype .= '?';
+      }
       $bundle[$bundle_name]['fields'][] = array(
-        'name' => $field_short,
-        'type' => $info['type'],
+        'name' => preg_replace('/^field_uib_/', '_', $field_name),
+        'type' => $ftype,
       );
     }
   }
