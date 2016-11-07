54bdf57746c6d76135bcf4c365892d39e315ed94
Are Johannessen <are.johannessen@uib.no>
2016-11-07 14:53:26 +0100

    RTS-16824 Added a second field collection to uib_study content type
    To be able to translate the relevant links list in a uib_study we need
    to add a separate list as the field collections are not translatable.
    Work includes:
    
    - added a new field collection with the same properties as the existing
    - added logic to choose between the field collections based on language
    - includes commit:636702b in SG

--- themes/uib_w3/template.php
+++ themes/uib_w3/template.php
@@ -556,7 +556,8 @@ EOD;
         ));
         $variables['page']['content']['study_so_code'] = __uib_w3__render_block('uib_study', 'study_so_code', 4);
         $variables['page']['content']['study_testimonial'] = __uib_w3__render_block('uib_study', 'study_testimonial', 10, TRUE);
-        $variables['page']['content_bottom']['field_uib_link_section'] = field_view_field('node', $variables['node'], 'field_uib_link_section', array(
+        $link_section = $language->language == 'en' ? '_2' : '';
+        $variables['page']['content_bottom']['field_uib_link_section'] = field_view_field('node', $variables['node'], 'field_uib_link_section' . $link_section, array(
           'label' => 'hidden',
           'weight' => 0,
         ));
