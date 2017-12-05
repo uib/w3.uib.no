0c6ee71d676f408e926e5dfcd79ce165cf5e8276
Pål Schroeder <pal.schroeder@uib.no>
2017-12-05 16:14:05 +0100

    RTS-19041 New alternative of showing contact info in areas
    - Added new field for visitor information
    - Placed visitor info field into colophon block
    - Hid visitor info field in template.php
    
    Includes commit:2362fa6 in SG

--- themes/uib_w3/template.php
+++ themes/uib_w3/template.php
@@ -1169,6 +1169,7 @@ function uib_w3_preprocess_node(&$variables, $hook) {
       'field_uib_study_image',
       'field_uib_study_relation',
       'field_uib_important_message',
+      'field_uib_visitor_info',
     );
     foreach ($hide_vars as $var) {
       hide($variables['content'][$var]);
