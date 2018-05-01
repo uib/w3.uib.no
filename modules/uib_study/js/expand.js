d594520334b12b4741e6ad499d7fc764dc04f2ce
Are Johannessen <are.johannessen@uib.no>
2018-05-01 23:26:55 +0200

    RTS-20241 Finalizing the study programme listing
    This includes:
    - implementing the alphabetical filter listing
    - minor tweaks to text strings in the grouped listing

--- modules/uib_study/js/expand.js
+++ modules/uib_study/js/expand.js
@@ -7,6 +7,81 @@
         heightStyle: 'content',
         icons: false
       });
+      $(document).ready(function() {
+        $('.page-studies-alphabetical #uib-study-filter-form').hide();
+      });
+      $('.page-studies-alphabetical .study-filter-form-form h2').click(function(event) {
+        $('.page-studies-alphabetical #uib-study-filter-form').toggle('blind');
+        $(this).toggleClass('expanded');
+      });
+      $('.page-studies-alphabetical #edit-submit').click(function(event) {
+        event.preventDefault();
+        var checked = getChecked();
+        if (checked.length > 0) {
+          showSelectedCategories(checked);
+        }
+        else {
+          showAllCategories();
+        }
+      });
+      $('.page-studies-alphabetical #uib-study-filter-form :checkbox').change(function(event) {
+        if ($(this).hasClass('study--all-categories')) {
+          showAll();
+        }
+        else {
+          showSelected();
+        }
+      });
+      function showAll() {
+        $('.page-studies-alphabetical #uib-study-filter-form :checkbox').prop('checked', false);
+        $('.page-studies-alphabetical #uib-study-filter-form .study--all-categories').prop('checked', true);
+      }
+      function showSelected() {
+        var checked = 0;
+        var checkBoxes = $('.page-studies-alphabetical #uib-study-filter-form :checkbox');
+        checkBoxes.each(function() {
+          if ($(this).prop('checked')) {
+            checked++;
+          }
+        });
+        if (checked) {
+          $('.page-studies-alphabetical #uib-study-filter-form .study--all-categories').prop('checked', false);
+        }
+        else {
+          $('.page-studies-alphabetical #uib-study-filter-form .study--all-categories').prop('checked', true);
+        }
+      }
+      function getChecked() {
+        var filters = ['bachelorprogram', 'masterprogram', 'arsstudium', 'integrertemasterpr-profesjonsst'];
+        var checked = [];
+        var checkBoxes = $('.page-studies-alphabetical #uib-study-filter-form :checkbox');
+        checkBoxes.each(function() {
+          if ($(this).prop('checked') == true) {
+            var thisCheckBox = $(this).attr('class');
+            filters.forEach(function(filter) {
+              if (thisCheckBox.indexOf(filter) > -1) {
+                checked.push(filter);
+              }
+            });
+          }
+        });
+        return checked;
+      }
+      function showSelectedCategories(checked) {
+        var studyCategories = $('.page-studies-alphabetical .study-index-alpha > div');
+        studyCategories.hide();
+        studyCategories.each(function() {
+          var sc = $(this);
+          checked.forEach(function(c) {
+            if (sc.hasClass('study-cat--' + c)) {
+              sc.show();
+            }
+          });
+        });
+      }
+      function showAllCategories() {
+        $('.page-studies-alphabetical .study-index-alpha > div').show();
+      }
     }
   };
 })(jQuery);
--- scripts/add-studies-alias.php
+++ scripts/add-studies-alias.php
@@ -1,7 +1,20 @@
 <?php
-  $study_alias = array(
+  $study_alias[0] = array(
     'source' => 'studies',
     'alias' => 'studier',
     'language' => 'nb',
   );
-  path_save($study_alias);
+  $study_alias[1] = array(
+    'source' => 'studies/alphabetical',
+    'alias' => 'studier/alfabetisk',
+    'language' => 'nb',
+  );
+  foreach ($study_alias as $sa) {
+    if (!drupal_lookup_path('source', $sa['alias'], 'nb')) {
+      path_save($sa);
+      uibx_log('Path ' . $sa['alias'] . ' saved');
+    }
+    else {
+      uibx_log('Path ' . $sa['alias'] . ' already exists and is not saved');
+    }
+  }
