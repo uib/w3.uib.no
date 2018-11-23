a8e5b155bca6849141fd5b688a5e4bd785a1b238
Are Johannessen <are.johannessen@uib.no>
2018-11-23 08:42:57 +0100

    Revert "RTS-21404 New way to handle image captions used in study nodes"
    This reverts commit db10a69431c8a8a2c580bf01711d4a6312d4dc76.

--- modules/uib_ckeditor_config/plugins/templates/uib_study_templates.js
+++ modules/uib_ckeditor_config/plugins/templates/uib_study_templates.js
@@ -10,6 +10,21 @@
     image: 'SITAT_MED_NAVN.svg',
     description: 'Relevant testimonial for this study',
     html: '(* testimonial *)'
+   },{
+    title: 'Portrait image with text',
+    image: 'PORTRETTFOTO_MED_BILDETEKST.svg',
+    description: 'This inserts a portrait image with text to the right',
+    html: '<div class="uib-study__portrait-image"><img src=" " width="100px" height="100px"><div class="uib-study__media-info"><p>Bilde tekst kommer her.</p></div></div>'
+   },{
+    title: 'Normal image with text',
+    image: 'NORMALBREDDEBILDE_MED_BILDETEKST.svg',
+    description: 'This inserts a image with text below',
+    html: '<div class="uib-study__image"><img src=" " width="100px" height="100px"><div class="uib-study__media-info"><p>Bilde tekst kommer her.</p></div></div>'
+   },{
+    title: 'Full width image with text',
+    image: 'FULLBREDDEBILDE_MED_BILDETEKST.svg',
+    description: 'This inserts a full width image with text below',
+    html: '<div class="uib-study__full-width-image"><img src=" " width="100px" height="100px"><div class="uib-study__media-info"><p>Bilde tekst kommer her.</p></div></div>'
    }
   ]
 });
