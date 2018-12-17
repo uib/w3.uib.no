43aa9bfcee74be5493e4714194e682be6d156352
Are Johannessen <are.johannessen@uib.no>
2018-12-17 15:07:25 +0100

    RTS-21382 Allow media and quotes in text
    We want to allow users to insert media and quotes with name in text for
    node type article. This implies:
    
    - adding template, media browser and responsive images to wysiwyg
    - adding image caption field to article
    - adjustments to ckeditor config
    - tweaks to function inserting image captions in text
    
    Includes commit:07f0656 from SG

---
+++ modules/uib_ckeditor_config/plugins/templates/uib_basic_templates.js
@@ -0,0 +1,9 @@
+CKEDITOR.addTemplates('uib_basic_templates',{
+  imagesPath: '/sites/all/modules/uib/uib_ckeditor_config/plugins/templates/images/',
+  templates:[{
+   title: 'Quote with name',
+   image: 'SITAT_MED_NAVN.svg',
+   description: 'Blockquote with name',
+   html: '<blockquote><p>Insert quote here</p><p class="uib-feature__quote-author">Insert author here</p></blockquote>'
+  }
+]});
--- modules/uib_ckeditor_config/plugins/templates/uib_templates.js
+++ modules/uib_ckeditor_config/plugins/templates/uib_templates.js
@@ -1,30 +1,29 @@
- CKEDITOR.addTemplates('uib_templates',{
-   imagesPath: '/sites/all/modules/uib/uib_ckeditor_config/plugins/templates/images/',
-   templates:[{
-     title: 'Fact box',
-     image: 'FAKTABOKS.svg',
-     description: 'Collapsible fact box',
-     html: '<div class="uib-feature__fact-box"><h2>Faktaboks</h2><div class="uib-feature__fact-box--facts collapsed"><p>Fakta kommer her</p></div></div>'
-   },{
-    title: 'Quote with name',
-    image: 'SITAT_MED_NAVN.svg',
-    description: 'Blockquote with name',
-    html: '<blockquote><p>Insert quote here</p><p class="uib-feature__quote-author">Insert author here</p></blockquote>'
-   },{
-    title: 'Portrait image with text',
-    image: 'PORTRETTFOTO_MED_BILDETEKST.svg',
-    description: 'This inserts a portrait image with text to the right',
-    html: '<div class="uib-feature__portrait-image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
-   },{
-    title: 'Normal image with text',
-    image: 'NORMALBREDDEBILDE_MED_BILDETEKST.svg',
-    description: 'This inserts a image with text below',
-    html: '<div class="uib-feature__image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
-   },{
-    title: 'Full width image with text',
-    image: 'FULLBREDDEBILDE_MED_BILDETEKST.svg',
-    description: 'This inserts a full width image with text below',
-    html: '<div class="uib-feature__full-width-image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
-   }
-  ]
-});
+CKEDITOR.addTemplates('uib_templates',{
+  imagesPath: '/sites/all/modules/uib/uib_ckeditor_config/plugins/templates/images/',
+  templates:[{
+    title: 'Fact box',
+    image: 'FAKTABOKS.svg',
+    description: 'Collapsible fact box',
+    html: '<div class="uib-feature__fact-box"><h2>Faktaboks</h2><div class="uib-feature__fact-box--facts collapsed"><p>Fakta kommer her</p></div></div>'
+  },{
+   title: 'Quote with name',
+   image: 'SITAT_MED_NAVN.svg',
+   description: 'Blockquote with name',
+   html: '<blockquote><p>Insert quote here</p><p class="uib-feature__quote-author">Insert author here</p></blockquote>'
+  },{
+   title: 'Portrait image with text',
+   image: 'PORTRETTFOTO_MED_BILDETEKST.svg',
+   description: 'This inserts a portrait image with text to the right',
+   html: '<div class="uib-feature__portrait-image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
+  },{
+   title: 'Normal image with text',
+   image: 'NORMALBREDDEBILDE_MED_BILDETEKST.svg',
+   description: 'This inserts a image with text below',
+   html: '<div class="uib-feature__image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
+  },{
+   title: 'Full width image with text',
+   image: 'FULLBREDDEBILDE_MED_BILDETEKST.svg',
+   description: 'This inserts a full width image with text below',
+   html: '<div class="uib-feature__full-width-image"><img src=" " width="100px" height="100px"><div class="uib-feature__media-info"><p>Bilde tekst kommer her.</p></div></div>'
+  }
+]});
