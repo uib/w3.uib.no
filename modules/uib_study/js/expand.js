99dfcc96876482abe2ead3098690b52a258e5eba
Are Johannessen <are.johannessen@uib.no>
2018-12-19 14:57:10 +0100

    RTS-21439 Solution to filtered by url in studies/alphabetical

--- modules/uib_study/js/expand.js
+++ modules/uib_study/js/expand.js
@@ -9,6 +9,7 @@
       });
       $(document).ready(function() {
         $('.page-studies-alphabetical #uib-study-filter-form').hide();
+        setInitChecked();
       });
       $('.page-studies-alphabetical .study-filter-form-form h2').click(function(event) {
         $('.page-studies-alphabetical #uib-study-filter-form').toggle('blind');
@@ -55,6 +56,19 @@
           $('.page-studies-alphabetical #uib-study-filter-form .study--all-categories').prop('checked', true);
         }
       }
+      function setInitChecked() {
+        var params = window.location.search.substring(1).split('&');
+        var cats = {bachelor: 'bachelorprogram', master: 'masterprogram', aar: 'arsstudium', masterprof: 'integrertemasterpr-profesjonsst'};
+        if (params[0].length > 0) {
+          var checked = [];
+          params.forEach(function(param) {
+            checked.push(cats[param]);
+            $('.page-studies-alphabetical #uib-study-filter-form .study--all-categories').prop('checked', false);
+            $('.page-studies-alphabetical #uib-study-filter-form .study--' + cats[param]).prop('checked', true);
+          });
+          showSelectedCategories(checked);
+        }
+      }
       function getChecked() {
         var filters = ['bachelorprogram', 'masterprogram', 'arsstudium', 'integrertemasterpr-profesjonsst'];
         var checked = [];
@@ -73,18 +87,28 @@
       }
       function showSelectedCategories(checked) {
         var studyCategories = $('.page-studies-alphabetical .study-index-alpha > div');
+        var newQueryString = ''
+        var categories = {bac: 'bachelor', mas: 'master', ars: 'aar', int: 'masterprof'};
         studyCategories.hide();
         studyCategories.each(function() {
           var sc = $(this);
           checked.forEach(function(c) {
             if (sc.hasClass('study-cat--' + c)) {
               sc.show();
+              newQueryString += categories[c.substr(0, 3)] + '&';
             }
           });
         });
+        if (history.pushState) {
+          history.replaceState(null, '', location.href.split('?')[0]);
+          history.replaceState(null, null, '?' + newQueryString.substr(0, newQueryString.length - 1));
+        }
       }
       function showAllCategories() {
         $('.page-studies-alphabetical .study-index-alpha > div').show();
+        if (history.pushState) {
+          history.replaceState(null, '', location.href.split('?')[0]);
+        }
       }
       function scrollToAnchor(id) {
         $('html, body').animate({scrollTop: $(id).offset().top}, 'slow');
