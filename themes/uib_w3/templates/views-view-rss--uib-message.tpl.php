ead8d334ff1377da8fd21083cba3732a9e07aea8
Amund Østvoll <amund.ostvoll@uib.no>
2019-01-11 12:39:20 +0100

    RTS-21581 Change channel link for rss message feed

--- themes/uib_w3/templates/views-view-rss--uib-message.tpl.php
+++ themes/uib_w3/templates/views-view-rss--uib-message.tpl.php
@@ -7,11 +7,17 @@
  * @ingroup views_templates
  */
 ?>
+<?php
+$arr = explode(' ',$channel_elements);
+$regex = '/https?\:\/\/[^\",]+/i';
+preg_match_all($regex, $channel_elements, $match);
+$channel_link = str_replace('.xml','',$match[0][0]);
+?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
-<rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
+<rss version="2.0" xml:base="<?php print $channel_link; ?>"<?php print $namespaces; ?>>
   <channel>
     <title><?php print $title; ?></title>
-    <link><?php print $link; ?></link>
+    <link><?php print $channel_link; ?></link>
     <description><?php print $description; ?></description>
     <language><?php print $langcode; ?></language>
     <?php print $channel_elements; ?>
