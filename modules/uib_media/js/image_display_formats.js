14f28e0b85b0fd653c45d256fc35df0d7c5ded78
Pål Schroeder <pal.schroeder@uib.no>
2019-05-09 12:11:41 +0200

    RTS-22367 Improved the 'Display as' dropdown for images and videos in editor
    - Added jQuery script that modifies the list
    - Attached the script to forms in uib_media.module

---
+++ modules/uib_media/js/image_display_formats.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+  $("[value=default]").remove();
+  $("[value=full_width_banner]").remove();
+  $("[value=teaser]").remove();
+  $("[value=preview]").remove();
+  $("[value=wysiwyg]").remove();
+  $("[value=full_width_image]").remove();
+  $("[value=wide_thumbnail]").remove();
+  $("[value=area_bottom]").remove();
+  $("[value=content_sidebar]").remove();
+  $("[value=content_main]").remove();
+  $("[value=area_main]").remove();
+  $("[value=feature_image]").remove();
+  full_width = $("[value=feature_article_full_width]").detach();
+  $("[value=feature_article_standard]").text(Drupal.t("Image or video (landscape orientation)"));
+  $("[value=feature_image_portrait]").text(Drupal.t("Image (portrait orientation)"));
+
+  if ($("#edit-field-uib-feature-article-und").attr("checked")) $("body").addClass("feature-article");
+  $("#edit-field-uib-feature-article-und").click(function() {
+    if ($( this ).prop("checked")) $("body").addClass("feature-article")
+    else $("body").removeClass("feature-article")
+  });
+
+  if ($("body.feature-article", window.parent.document).html()[0]) {
+    $("[value=feature_article_standard]").text(Drupal.t("Feature article image or video (landscape orientation)"));
+    $("[value=feature_image_portrait]").text(Drupal.t("Feature article image (portrait orientation)"));
+    $("#edit-format").append(full_width);
+    $("[value=feature_article_full_width]").text(Drupal.t("Feature article image or video (full width)"));
+    // Firefox needs the form to be reset
+    $('#media-wysiwyg-format-form').trigger("reset");
+  }
+});
