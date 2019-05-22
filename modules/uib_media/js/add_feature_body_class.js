uib/w3.uib.no

commit 14f28e0b85b0fd653c45d256fc35df0d7c5ded78

RTS-22367 Improved the 'Display as' dropdown for images and videos in editor
- Added jQuery script that modifies the list
- Attached the script to forms in uib_media.module

modules/uib_media/js/image_display_formats.js
@@ -1,0 +1,32 @@
+jQuery(function($){
+  $("[value=default]").remove();
+  $("[value=full_width_banner]").remove();
+  $("[value=teaser]").remove();
+  $("[value=preview]").remove();
+  $("[value=wysiwyg]").remove();
+  $("[value=full_width_image]").remove();
+  $("[value=wide_thumbnail]").remove();
+  $("[value=area_bottom]").remove();
+  $("[value=content_sidebar]").remove();
+  $("[value=content_main]").remove();
+  $("[value=area_main]").remove();
+  $("[value=feature_image]").remove();
+  full_width = $("[value=feature_article_full_width]").detach();
+  $("[value=feature_article_standard]").text(Drupal.t("Image or video (landscape orientation)"));
+  $("[value=feature_image_portrait]").text(Drupal.t("Image (portrait orientation)"));
+
+  if ($("#edit-field-uib-feature-article-und").attr("checked")) $("body").addClass("feature-article");
+  $("#edit-field-uib-feature-article-und").click(function() {
+    if ($( this ).prop("checked")) $("body").addClass("feature-article")
+    else $("body").removeClass("feature-article")
+  });
+
+  if ($("body.feature-article", window.parent.document).html()[0]) {
+    $("[value=feature_article_standard]").text(Drupal.t("Feature article image or video (landscape orientation)"));
+    $("[value=feature_image_portrait]").text(Drupal.t("Feature article image (portrait orientation)"));
+    $("#edit-format").append(full_width);
+    $("[value=feature_article_full_width]").text(Drupal.t("Feature article image or video (full width)"));
+    // Firefox needs the form to be reset
+    $('#media-wysiwyg-format-form').trigger("reset");
+  }
+});

modules/uib_media/uib_media.module
@@ -79,3 +79,10 @@ function uib_media__find_media_in_text($text, $captions, $num = 0, $text_with_ca
     return $text_with_captions . $text;
   }
 }
+
+function uib_media_form_alter(&$form, &$form_state, $form_id) {
+  $form['#attached']['js'][] = array(
+    'data' => drupal_get_path('module', 'uib_media') . '/js/image_display_formats.js',
+    'type' => 'file',
+  );
+}

commit bc764cd6e404ce4da967c5589fb422baae089989

RTS-22409 Fix for javascript failure in image_display_formats.js
The javascript introduced in commit:14f28e0b was not contained to the
correct form. It was attached to any form causing javascript failures in
other scripts as well. The most critical failure was the chatbot hubro.

By splitting the javascript functionality into two scripts and attaching
the functions to the correct forms the error was fixed.

modules/uib_media/js/add_feature_body_class.js
@@ -1,0 +1,11 @@
+(function ($) {
+  Drupal.behaviors.addFeatureBodyClass = {
+    attach: function (context, settings) {
+      if ($("#edit-field-uib-feature-article-und").attr("checked")) $("body").addClass("feature-article");
+      $("#edit-field-uib-feature-article-und").click(function() {
+        if ($( this ).prop("checked")) $("body").addClass("feature-article")
+        else $("body").removeClass("feature-article")
+      });
+    }
+  }
+})(jQuery);

modules/uib_media/js/image_display_formats.js
@@ -1,32 +1,30 @@
-jQuery(function($){
-  $("[value=default]").remove();
-  $("[value=full_width_banner]").remove();
-  $("[value=teaser]").remove();
-  $("[value=preview]").remove();
-  $("[value=wysiwyg]").remove();
-  $("[value=full_width_image]").remove();
-  $("[value=wide_thumbnail]").remove();
-  $("[value=area_bottom]").remove();
-  $("[value=content_sidebar]").remove();
-  $("[value=content_main]").remove();
-  $("[value=area_main]").remove();
-  $("[value=feature_image]").remove();
-  full_width = $("[value=feature_article_full_width]").detach();
-  $("[value=feature_article_standard]").text(Drupal.t("Image or video (landscape orientation)"));
-  $("[value=feature_image_portrait]").text(Drupal.t("Image (portrait orientation)"));
+(function ($) {
+  Drupal.behaviors.imageDisplayFormats = {
+    attach: function (context, settings) {
+      $("[value=default]").remove();
+      $("[value=full_width_banner]").remove();
+      $("[value=teaser]").remove();
+      $("[value=preview]").remove();
+      $("[value=wysiwyg]").remove();
+      $("[value=full_width_image]").remove();
+      $("[value=wide_thumbnail]").remove();
+      $("[value=area_bottom]").remove();
+      $("[value=content_sidebar]").remove();
+      $("[value=content_main]").remove();
+      $("[value=area_main]").remove();
+      $("[value=feature_image]").remove();
+      full_width = $("[value=feature_article_full_width]").detach();
+      $("[value=feature_article_standard]").text(Drupal.t("Image or video (landscape orientation)"));
+      $("[value=feature_image_portrait]").text(Drupal.t("Image (portrait orientation)"));
 
-  if ($("#edit-field-uib-feature-article-und").attr("checked")) $("body").addClass("feature-article");
-  $("#edit-field-uib-feature-article-und").click(function() {
-    if ($( this ).prop("checked")) $("body").addClass("feature-article")
-    else $("body").removeClass("feature-article")
-  });
-
-  if ($("body.feature-article", window.parent.document).html()[0]) {
-    $("[value=feature_article_standard]").text(Drupal.t("Feature article image or video (landscape orientation)"));
-    $("[value=feature_image_portrait]").text(Drupal.t("Feature article image (portrait orientation)"));
-    $("#edit-format").append(full_width);
-    $("[value=feature_article_full_width]").text(Drupal.t("Feature article image or video (full width)"));
-    // Firefox needs the form to be reset
-    $('#media-wysiwyg-format-form').trigger("reset");
+      if ($("body.feature-article", window.parent.document).html()) {
+        $("[value=feature_article_standard]").text(Drupal.t("Feature article image or video (landscape orientation)"));
+        $("[value=feature_image_portrait]").text(Drupal.t("Feature article image (portrait orientation)"));
+        $("#edit-format").append(full_width);
+        $("[value=feature_article_full_width]").text(Drupal.t("Feature article image or video (full width)"));
+        // Firefox needs the form to be reset
+        $('#media-wysiwyg-format-form').trigger("reset");
+      }
+    }
   }
-});
+})(jQuery);

modules/uib_media/uib_media.module
@@ -81,8 +81,16 @@ function uib_media__find_media_in_text($text, $captions, $num = 0, $text_with_ca
 }
 
 function uib_media_form_alter(&$form, &$form_state, $form_id) {
-  $form['#attached']['js'][] = array(
-    'data' => drupal_get_path('module', 'uib_media') . '/js/image_display_formats.js',
-    'type' => 'file',
-  );
+  if ($form['#form_id'] == 'media_wysiwyg_format_form') {
+    $form['#attached']['js'][] = array(
+      'data' => drupal_get_path('module', 'uib_media') . '/js/image_display_formats.js',
+      'type' => 'file',
+    );
+  }
+  if ($form['#form_id'] == 'uib_article_node_form') {
+    $form['#attached']['js'][] = array(
+      'data' => drupal_get_path('module', 'uib_media') . '/js/add_feature_body_class.js',
+      'type' => 'file',
+    );
+  }
 }